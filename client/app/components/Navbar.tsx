00aa358119e35152c55882e8ffe9e7e4e8a3df16
Clyde Geyer <geyer.clyde@gmail.com>
2026-02-26 21:26:13 -0500

    connect logout button to endpoint

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Bell, LogOut, LayoutDashboard, Database, Settings as SettingsIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { apiRequest } from '../utils/api-request';
 
 interface NavbarProps {
   currentPage: string;
@@ -10,8 +11,12 @@ interface NavbarProps {
 export function Navbar({ currentPage, onNavigate }: NavbarProps) {
   const router = useRouter();
   const handleLogout = () => {
-    localStorage.removeItem('isLoggedIn');
-    router.push('/login');
+    try {
+      apiRequest(`/api/login/logout`, `POST`);
+      router.push('/login');
+    } catch (error: any) {
+      console.log(error);
+    }
   };
   return (
     <div className="relative z-20 bg-white/80 backdrop-blur-sm border-b border-gray-200">
@@ -20,34 +25,31 @@ export function Navbar({ currentPage, onNavigate }: NavbarProps) {
         <div className="flex items-center gap-4">
           <button
             onClick={() => onNavigate('dashboard')}
-            className={`px-6 py-2 rounded transition flex items-center gap-2 ${
-              currentPage === 'dashboard' 
-                ? 'bg-[#ffd966] text-gray-800' 
-                : 'bg-transparent text-gray-600 hover:bg-gray-100'
-            }`}>
+            className={`px-6 py-2 rounded transition flex items-center gap-2 ${currentPage === 'dashboard'
+              ? 'bg-[#ffd966] text-gray-800'
+              : 'bg-transparent text-gray-600 hover:bg-gray-100'
+              }`}>
             <LayoutDashboard size={18} />
           </button>
           <button
             onClick={() => onNavigate('data')}
-            className={`px-6 py-2 rounded transition flex items-center gap-2 ${
-              currentPage === 'data' 
-                ? 'bg-[#ffd966] text-gray-800' 
-                : 'bg-transparent text-gray-600 hover:bg-gray-100'
-            }`}>
+            className={`px-6 py-2 rounded transition flex items-center gap-2 ${currentPage === 'data'
+              ? 'bg-[#ffd966] text-gray-800'
+              : 'bg-transparent text-gray-600 hover:bg-gray-100'
+              }`}>
             <Database size={18} />
           </button>
           <button
             onClick={() => onNavigate('settings')}
-            className={`px-6 py-2 rounded transition flex items-center gap-2 ${
-              currentPage === 'settings' 
-                ? 'bg-[#ffd966] text-gray-800' 
-                : 'bg-transparent text-gray-600 hover:bg-gray-100'
-            }`}>
+            className={`px-6 py-2 rounded transition flex items-center gap-2 ${currentPage === 'settings'
+              ? 'bg-[#ffd966] text-gray-800'
+              : 'bg-transparent text-gray-600 hover:bg-gray-100'
+              }`}>
             <SettingsIcon size={18} />
           </button>
         </div>
 
-       
+
         <div className="flex items-center gap-3">
           {/* <button 
             className="p-2 hover:bg-gray-100 rounded-full transition"
