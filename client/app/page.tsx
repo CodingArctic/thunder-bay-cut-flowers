d12866531a5de266593600fafc65f5bacc333f34
Clyde Geyer <geyer.clyde@gmail.com>
2026-02-26 20:40:12 -0500

    remove logged in state for root page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { Navbar } from "./components/Navbar";
 import { Dashboard } from "./components/Dashboard";
 import { Data } from "./components/Data";
@@ -8,26 +7,6 @@ import { Settings } from "./components/Settings";
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState('dashboard');
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-
-  useEffect(() => {
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-
-    if (!isLoggedIn) {
-      router.push('/login');
-      return;
-    } else {
-      setLoading(false);
-    }
-  }, [router]);
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="w-16 h-16 border-4 border-t-4 border-gray-500 border-solid rounded-full animate-spin"></div>
-      </div>
-    );
-  }
 
   return (
     <div className="min-h-screen bg-[#f5f0eb] relative overflow-hidden">
